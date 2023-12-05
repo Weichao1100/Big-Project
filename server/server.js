f87e08ed937910e44f85dc352fbd10c7dcdb2083
wliang21 <122560519+Weichao1100@users.noreply.github.com>
2023-12-05 04:08:49 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -66,6 +66,7 @@ app.get("/setValue", function (req, res) {
 	db.collection("dataWeather").insert(dataObj, function(err,result){
 		console.log("added data: " + JSON.stringify(dataObj));
 	});
+	console.log('set vaulue-------->>',VALUEt.toString() + " " + VALUEh + " " + VALUEtime)
   res.send(VALUEt.toString() + " " + VALUEh + " " + VALUEtime + "\r");
 });
 
