e7370b20d6377fe819eb61667ca1b9be9fc88ebb
wliang21 <122560519+Weichao1100@users.noreply.github.com>
2023-12-05 05:50:52 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get("/getData", function (req, res) {
   var from = parseInt(req.query.from);
   var to = parseInt(req.query.to);
   db.collection("dataWeather").find({time:{$gt:from, $lt:to}}).sort({time:-1}).toArray(function(err, result){
-    res.send(JSON.stringify(result[0]));
+    res.json(result[0]);
   });
 });
 
